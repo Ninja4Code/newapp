df5cbaa0ad20cb8dbfdcc5ee07b56bbc0f74a82e
Charles Owen <owen.charlesr@hotmail.com>
2019-01-20 20:25:22 -0500

    Adding Shared Folder

---
+++ components/shared/baseUrl.js
@@ -0,0 +1,3 @@
+//export const baseUrl = 'http://localhost:3001/'
+export const baseUrl = 'http://www.excellorsoftware.com/Images/';
+export const appUrl = 'http://www.excellorsoftware.com/Confusion/';
