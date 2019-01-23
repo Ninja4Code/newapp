341fd3a5077213b88afc8ea8334fa968ce4899e6
Charles Owen <owen.charlesr@hotmail.com>
2019-01-22 21:05:30 -0500

    Taking out logger from store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,6 +1,5 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
@@ -16,7 +15,7 @@ export const ConfigureStore = () => {
             leaders, 
             favorites
         }),
-        applyMiddleware(thunk, logger)
+        applyMiddleware(thunk)
     );
 
     return store;
